b5083787111c97876f00dc8ff9e07e42206f9e4d
Jeyms <james.boac@gmail.com>
2025-06-23 04:37:08 +0800

    Added frontend domain to CORS

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,7 @@ app.use(
       'http://localhost:5173',
       'https://wings-backend.vercel.app',
       'https://wings-wine.vercel.app',
+      'https://wings-irjdpus4o-poopyy17s-projects.vercel.app',
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
